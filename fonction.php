76b7007f46666d308c54c5824c70bc1dcfa31eb2
Ali <mohmedalims24@gmail.com>
2020-05-20 14:27:44 +0200

    function Number

---
+++ fonction.php
@@ -0,0 +1,14 @@
+<?php
+//             ----  une fonction qui prend en parametre un nombre et qui et qui affiche
+//     les nombres de 0 jusqu'a ce nombre    --
+
+
+function number($x)
+{
+
+    for ($i = 0; $i < $x; $i++) {
+        echo $i;
+    }
+
+}
+?>
